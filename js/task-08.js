141690b35a3240c9a7f1c519978ccd88011982e5
oleg <oleg.poviychuk@gmail.com>
2023-10-02 00:23:16 +0300

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,19 @@
+const formDef = document.querySelector(`.login-form`);
+// console.log(formDef);
 
+formDef.addEventListener(`submit`, function (event) {
+  event.preventDefault();
+  const emailDef = formDef.elements.email;
+  const passwordDef = formDef.elements.password;
+  if (emailDef.value.length === 0 || passwordDef.value.length === 0) {
+    alert(`Ви повинні заповнити всі поля`);
+    return;
+  }
+  const formObt = {
+    email: emailDef.value,
+    password: passwordDef.value,
+  };
+
+  console.log(formObt);
+  formDef.reset;
+});
