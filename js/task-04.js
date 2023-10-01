df431ca6f066e07d9e3a6fff02627034d5ffdb2a
oleg <oleg.poviychuk@gmail.com>
2023-10-01 22:20:30 +0300

    task5

--- js/task-04.js
+++ js/task-04.js
@@ -2,11 +2,7 @@ const counterValue = document.querySelector("#value");
 const plus = document.querySelector(`button[data-action="increment"]`);
 const minus = document.querySelector(`button[data-action="decrement"]`);
 let counter = 0;
-console.log(plus);
-console.log(minus);
-console.log(counterValue);
 
-// plus.addEventListener("clickPl", clickPlus);
 plus.addEventListener("click", () => {
   counter += 1;
   counterValue.textContent = counter;
