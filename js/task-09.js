52e29a50e1bf9495f44fc52333f18ad5fb658ea2
oleg <oleg.poviychuk@gmail.com>
2023-10-02 00:46:03 +0300

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,13 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+const colorWrite = document.querySelector(`.color`);
+const buttonColor = document.querySelector(`.change-color`);
+const body = document.querySelector(`body`);
+// console.log(bodyDef);
+
+buttonColor.addEventListener(`click`, () => {
+  const ranColor = getRandomHexColor();
+  body.style.backgroundColor = `${ranColor}`;
+  colorWrite.textContent = `${ranColor}`;
+});
